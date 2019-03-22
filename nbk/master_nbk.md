5317d26efbca6c49137907791534940bc759ea78
Dag Tanneberg <dag.tanneberg@googlemail.com>
2019-03-22 05:56:18 +0100

    Replicate Table 4 results

---
+++ nbk/master_nbk.md
@@ -0,0 +1,13 @@
+## 03/21/2019
+
+Package plm's within estimator exactly reproduces the results published in Table 4.
+The next step should be to assess model fit using the most informative 
+formulation, i.e. Model 4. Note that the standard errors could be reproduced 
+exactly as the text is somewhat vague on what robust standard errors were used.
+Anyhow, the different versions supplied by plm did not lead to substantively
+different conclusions.
+
+Proposed EDA:
+    1. Frequency and electoral success of PECs per country (over time)
+    2. Jackknife: Do the results depend on particular countries?
+    3. Increase precision: Replace PR/Plurality by District Magnitude
--- src/r/replication/01_replicate_using_plm.R
+++ src/r/replication/01_replicate_using_plm.R
@@ -1,26 +1,44 @@
+# This script replicates Model 1 through 4 from Table 4 in the original publication.
+# Author: Dag Tanneberg
+# Date: 03/21/2019
+# ================================================================================
+# Prepare workspace
 rm(list = ls()[!(ls() %in% clean_workspace)])
-library(foreign)
+packages <- c("foreign", "plm")
+for(i in packages){
+    if(!(i %in% rownames(installed.packages()))) {
+        cat("Now installing required package:\\t", i)
+        install.packages(i, dependencies = TRUE)
+    }
+    library(i, character.only = TRUE)
+}
+
+# Declare string constants
+panel_id <- c("country", "year2")
+control <- paste(
+    c("enep", "disprop * pr", "plurality * closeness", "growth", "lnincome"),
+    collapse = " + "
+)
+treatment <- c("pec1", "pec20", "vote_pec", "smallpec+largepec")
+response <- "turnout"
+
+# Load and prepare data
 tillman <- read.dta(
-    file.path( path_project, "prestudies", "ellger", "tilmann_merger.dta" )
+    file.path(path_project, "prestudies", "ellger", "tilmann_merger.dta")
 )
-detach(package:foreign)
-data_to_fit <- subset(tillman,
-    select = c("country", "year2", "turnout", "pec1", "enep", "disprop", "pr",
-        "plurality", "closeness", "growth", "lnincome"
+data_to_fit <- pdata.frame(tillman, index = c("country", "year2"))
+
+fitted_models <- list()
+for(i in 1:length(treatment)) {
+    assign(
+        "frm",
+        paste0(response, " ~ ", paste(treatment[i], control, sep = " + "))
     )
-)
-
-library("plm")
-data_to_fit <- pdata.frame(data_to_fit, index = c("country", "year2"))
-summary(data_to_fit)
-
-
-head(data_to_fit)
-fit <- plm(
-    turnout ~ pec1 +
-        enep + disprop * pr + plurality * closeness + growth + lnincome,
-    model = "within",
-    data = data_to_fit
-)
-summary(fit)
-detach(package:plm)
+    fitted_models[[i]] <- plm(
+        as.formula(frm), model = "within", data = data_to_fit
+    )
+}
+names(fitted_models) <- treatment
+lapply(fitted_models, coef)
+## END
+## Coefficients replicate exactly.
