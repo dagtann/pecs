c8e53b1f6050dfee4ec749f005a7fee355cb6875
Dag Tanneberg <dag.tanneberg@googlemail.com>
2019-03-27 16:52:22 +0100

    Update project administration files

--- src/r/master.R
+++ src/r/master.R
@@ -2,11 +2,16 @@ rm(list = ls())
 options(help_type = "html")
 path_project <- file.path("~", "github", "pecs")
 
-packs <- c("tidyverse")
+packs <- c("tidyverse", "foreign")
 for( i in packs ) {
     if(!(i %in% rownames( installed.packages())))
         installed.packages(i, dependencies = TRUE)
     library(i, character.only = TRUE)
 }
 rm(packs)
-clean_workspace <- c(ls(), "clean_workspace")
+
+tillman <- read.dta(
+    file.path(path_project, "prestudies", "ellger", "tilmann_merger.dta")
+)
+detach(package:foreign)
+cleanWorkspace <- c(ls(), "cleanWorkspace")
