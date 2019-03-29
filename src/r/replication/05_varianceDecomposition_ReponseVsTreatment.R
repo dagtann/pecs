e292f4d6d6913a9d2c29dd7a8ac75bd573b39925
Dag Tanneberg <dag.tanneberg@googlemail.com>
2019-03-29 16:26:48 +0100

    Update variance decomposition and jackknife

--- src/r/replication/05_varianceDecomposition_ReponseVsTreatment.R
+++ src/r/replication/05_varianceDecomposition_ReponseVsTreatment.R
@@ -1,24 +1,55 @@
-
+library("gridExtra")
 btw_dta <- select(tillman, country, pec1, vote_pec, turnout) %>%
     group_by(country) %>%
     summarise_all(.funs = mean, na.rm = TRUE) %>%
     rename(bw_pec1 = pec1, bw_vote_pec = vote_pec, bw_turnout = turnout)
 tillman <- left_join(tillman, btw_dta, by = "country")
 for(v in c("pec1", "vote_pec", "turnout")){
-    tillman[, paste("wi", v, sep = "_")] <- tillman[, v] - 
-        tillman[, paste("bw", v, sep = "_")]
+    tillman[, paste("wi", v, sep = "_")] <-
+        tillman[, v] - tillman[, paste("bw", v, sep = "_")]
 }
 
-# (a) vote_pec -------------------------------------------------------------------
-ggplot(data = tillman, aes(x = wi_pec1, y = wi_turnout)) + 
-    geom_point() +
-    geom_smooth(se = FALSE) +
-    geom_smooth(method = "lm", col = "red") +
-    facet_wrap(~ country)
+# (a) pec1 -----------------------------------------------------------------------
+countries <- unique(tillman[["country"]])
+r <- vector(mode = "numeric", length = length(countries))
+names(r) <- countries
+for(i in countries){
+    filter <- which(tillman[, "country"] == i)
+    r[i] <- cor(
+        tillman[filter, "wi_turnout"], tillman[filter, "wi_pec1"],
+        use = "complete.obs"
+    )
+}
+r[is.na(r)] <- 999
+tillman <- within(tillman,
+    country_f <- factor(country, levels = countries[order(r)])
+)
+p1 <- ggplot(data = tillman, aes(x = wi_pec1, y = wi_turnout)) + 
+    geom_smooth(method = "lm", se = FALSE) +
+    geom_point(alpha = .6) +
+    facet_wrap(~ country_f, ncol = 3) +
+    labs(x = "PEC in election?", y = "Turnout")
 
-ggplot(data = tillman, aes(x = wi_vote_pec, y = wi_turnout)) + 
-    geom_point() +
-    geom_smooth(se = FALSE) +
-    geom_smooth(method = "lm", col = "red") +
-    facet_wrap(~ country)
+# (b) vote_pec
+for(i in countries){
+    filter <- which(tillman[, "country"] == i)
+    r[i] <- cor(
+        tillman[filter, "wi_turnout"], tillman[filter, "wi_vote_pec"],
+        use = "complete.obs"
+    )
+}
+tillman <- within(tillman,
+    country_f <- factor(country, levels = countries[order(r)])
+)
+p2 <- ggplot(data = tillman, aes(x = wi_vote_pec, y = wi_turnout)) + 
+    geom_smooth(method = "lm", se = FALSE) +
+    geom_point(alpha = .6) +
+    facet_wrap(~ country_f, ncol = 3) +
+    labs(x = "Percent of vote for PEC", y = "Turnout")
+p_total <- grid.arrange(p1, p2, ncol = 2)
+ggsave(
+    file.path(path_project, "out", "scatter_VarianceDecomposition_TreatmentVsOutcome.png"),
+    p_total
+)
+
 ## END
--- src/r/replication/06_jacknife_fe.R
+++ src/r/replication/06_jacknife_fe.R
@@ -1,5 +1,4 @@
-library("plm")
-
+library("lmtest")
 
 execute_jackknife <- function(drop, from, formula, data){
     # drop = "France"
@@ -7,7 +6,7 @@ execute_jackknife <- function(drop, from, formula, data){
     # data = data_to_fit
     model_data <- data[data[[from]] != drop, ]
     fit <- plm(as.formula(formula), data = model_data, model = "within")
-    cbind(beta = coef(fit), se = sqrt(diag(vcov(fit))))
+    cbind(beta = coef(fit), se = sqrt(diag(vcovBK(fit, "HC3", "group"))))
 }
 
 # Declare string constants
@@ -22,52 +21,70 @@ country_entries <- sort(unique(tillman$country))
 
 # Prepare data
 data_to_fit <- pdata.frame(tillman, index = c("country", "year2"))
+fit <- plm(
+    as.formula(paste0(response, " ~ ", paste(treatment[1], control, sep = " + "))),
+    model = "within",
+    data = data_to_fit
+)
 
 # execute jacknife
-jackknife_pec1 <- lapply(country_entries, function(c) {
-    execute_jackknife(
-        drop = c,
-        from = "country",
-        formula = paste0(response, " ~ ", paste(treatment[2], control, sep = " + ")),
-        data = data_to_fit
+for(i in treatment){
+    assign(paste("results", i, sep = "_"),  lapply(
+        country_entries, function(c) {
+            execute_jackknife(
+                drop = c, from = "country",
+                formula = paste0(response, " ~ ", paste(i, control, sep = " + ")),
+                data = data_to_fit
+            )
+            }
+        )
     )
-    }
+}
+jackknife_results <- list(pec1 = results_pec1, vote_pec = results_vote_pec)
+rm(list = ls()[grep("results_", ls(), fixed = TRUE)])
+pdta <- cbind(
+    sapply(jackknife_results[[1]], function(x){x["pec1", ]}),
+    sapply(jackknife_results[[2]], function(x){x["vote_pec", ]})
 )
-names(jackknife_pec1) <- country_entries
-plot_data <- do.call(rbind.data.frame, jackknife_pec1)
-split_pos <- str_locate(rownames(plot_data), "\\.")[, 1]
-plot_data[, "country"] <- str_sub(
-    rownames(plot_data),
-    end = split_pos - 1
+pdta <- t(pdta)
+pdta <- cbind.data.frame(
+    pdta, country = rep(country_entries, 2),
+    treatment = rep(treatment, each = length(country_entries))
 )
-plot_data[, "term"] <- str_sub(
-    rownames(plot_data),
-    start = split_pos + 1
-)
-rownames(plot_data) <- NULL
-plot_data <- mutate(plot_data,
-    upper = beta + qnorm(1 - .05 / 2) * se,
-    lower = beta - qnorm(1 - .05 / 2) * se
-)
-coef_reference <- coef(
-    plm(
-        as.formula(paste0(response, " ~ ", paste(treatment[2], control, sep = " + "))),
-        data = data_to_fit,
-        model = "within"
-    )
-)
-coef_reference <- data.frame(
-    term = names(coef_reference), reference = coef_reference
-)
-plot_data <- left_join(plot_data, coef_reference, by = "term")
+pdta <- mutate(pdta, lower = beta - qnorm(.975) * se) %>%
+    mutate(upper = beta + qnorm(.975) * se)
 
-ggplot(data = plot_data,
+coef_reference <- vector("numeric", length(treatment))
+names(coef_reference) <- treatment
+for(i in treatment){
+    coef_reference[i] <- coef(
+        plm(
+            as.formula(paste0(response, " ~ ", paste(i, control, sep = " + "))),
+            data = data_to_fit,
+            model = "within"
+        )
+    )[i]
+}
+coef_reference <- data.frame(
+    treatment = names(coef_reference), reference = coef_reference
+)
+pdta <- left_join(pdta, coef_reference, by = "treatment")
+pdta <- mutate(pdta, treatment_f = factor(treatment, levels = c("pec1", "vote_pec"), labels = c("PEC in election?", "Percent of vote for PEC")))
+p <- ggplot(data = pdta,
     aes(x = country, y = beta, ymin = lower, ymax = upper)
 ) +
-    geom_hline(aes(yintercept = reference), col = "red") +
+    geom_hline(
+        aes(yintercept = reference, color = "Published estimate")
+    ) +
     geom_pointrange() +
-    facet_wrap(~term, scales = "free_y") +
+    facet_wrap(~treatment_f, scales = "free_y", ncol = 1) +
+    labs(y = "Estimated effect & CI95\nwhen country is dropped") +
     theme(
-        axis.text.x = element_text(angle = 45)
+        axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+        axis.title.x = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_blank()
     )
-
+ggsave(file.path(path_project, "out", "pointrange_jackknife_treatments.png"))
+# housekeeping
+detach(package:plm)
