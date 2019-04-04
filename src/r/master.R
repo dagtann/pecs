80ccc9e4bd713b1c4c1b5de75787f5593845772e
Dag Tanneberg <dag.tanneberg@googlemail.com>
2019-04-04 21:32:07 +0200

    update admin files

--- src/r/master.R
+++ src/r/master.R
@@ -1,21 +1,24 @@
+# Prepare workspace
 rm(list = ls())
 options(help_type = "html")
 
 path_project <- file.path("~", "github", "pecs")
-base_size <- 18
+base_size <- 18 # plot character size
 
-packs <- c("tidyverse", "foreign")
+packs <- c("tidyverse")
 for ( i in packs ) {
     if (!(i %in% rownames( installed.packages())))
         installed.packages(i, dependencies = TRUE)
     library(i, character.only = TRUE)
 }
-rm(packs)
+rm(packs, i)
 
-tillman <- read.dta(
-    file.path(path_project, "prestudies", "ellger", "tilmann_merger.dta")
+clean_workspace <- c(ls(), "clean_workspace")
+
+# Data munging
+munging_files <- list.files(
+    file.path(path_project, "src", "r", "data_munging"), full.names = TRUE
 )
-tillman <- mutate(tillman, turnout = turnout / 100, vote_pec = vote_pec / 100)
-detach(package:foreign)
-
+lapply(munging_files, source)
+# housekeeping
 clean_workspace <- c(ls(), "clean_workspace")
