3684b80c1fd956d6c7f48336ba5d47a8d7e4f7e4
Dag Tanneberg <dag.tanneberg@googlemail.com>
2019-05-31 06:20:50 +0200

    Update article draft, 1st steps to generate additional variables

--- src/r/data_munging/05_generate_additional variables.R
+++ src/r/data_munging/05_generate_additional variables.R
@@ -1,4 +1,8 @@
+if (FALSE) { # code to be deployed
 rm(list = ls()[!(ls() %in% clean_workspace)])
+packs <- c("lubridate")
+for (p in packs) library(p, character.only = TRUE)
+
 # Generate smallpec / largepec indicators
 pecs <- read_dta(
     file.path(path_project, "dta", "raw", "PEC_raw_stable15_2.dta"),
@@ -34,8 +38,37 @@ with(country_panel, table(smallpec, smallpec_neu))
 with(country_panel, table(largepec, largepec_neu))
 
 # Create cabinet member indicator
-cabinet <- readxl::read_excel(
+cabinets <- readxl::read_excel(
     file.path(path_project, "dta", "raw", "parlgov-stable.xlsx"), sheet = "cabinet"
+) %>% 
+mutate(
+    start_date = ymd(as.character(start_date)),
+    election_date = ymd(as.character(election_date))
 )
+tmp <- select(cabinets, cabinet_id, party_id, cabinet_party) %>%
+    rename(previous_cabinet_id = cabinet_id)
+table(is.na(tmp$previous_cabinet_id))
+tmp <- left_join(pecs, tmp, by = c("previous_cabinet_id", "party_id"))
+table(is.na(tmp$cabinet_party))
+for (i in pec_indicators) {
+    cat("Processing on:", i, "\n")
+    mask <- pecs[, i] == 1
+    assign("tmp2",
+        aggregate(cabinet_party ~ election_id, data = tmp[mask, ],
+            FUN = function(x) all(x, na.rm = TRUE)
+        )
+    )
+    print(table(tmp2[, 2], exclude = NULL))
+    print(table(pecs[mask, paste(i, "incumbent", sep = "_")]))
+    # cat("Result has shape", nrow(tmp), "entries.\n")
+    # names(tmp)[2] <- paste(i, "share", sep = "_")
+    # print(head(tmp))
+    # country_panel <- left_join(country_panel, tmp, by = "election_id")
+}
 
-rm(list = ls()[!(ls() %in% clean_workspace)])
+table(is.na(cabinets$cabinet_party))
+nrow(pecs)
+nrow(tmp)
+table(is.na(tmp$cabinet_party))
+tmp[is.na(tmp$cabinet_party), c("country_name_short", "election_date", "party_name_short")]
+}
